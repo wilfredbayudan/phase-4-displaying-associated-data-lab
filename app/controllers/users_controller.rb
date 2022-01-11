e7cf3cac6a2ef36c39a4752a8d280686b85c6eeb
Wilfred <wilfredbayudan@gmail.com>
2022-01-10 21:14:26 -1000

    commit tree e5df25726b585a630e8c23c823361690aa847f79

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def show
+    user = get_user
+    render json: user, include: :items
+  end
+
+  private
+
+  def get_user
+    User.find(params[:id])
+  end
+
 end
